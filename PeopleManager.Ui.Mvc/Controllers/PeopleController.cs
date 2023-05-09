9710fa321b89a1720e52b9539aafcca77befc38e
Nevix <126560882+SaimenVives@users.noreply.github.com>
2023-05-09 16:42:24 +0200

    Initial commit

---
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Ui.Mvc.Core;
+using PeopleManager.Ui.Mvc.Models;
+
+namespace PeopleManager.Ui.Mvc.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private readonly PeopleManagerDbContext _dbContext;
+
+        public PeopleController(PeopleManagerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var people = _dbContext.People.ToList();
+            return View(people);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Person person)
+        {
+            _dbContext.People.Add(person);
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
